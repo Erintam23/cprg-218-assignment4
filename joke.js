755c32e0dd93d5b2a86fc53b516dd8d8dcd35fdc
Erintam23 <erin.tam@edu.sait.ca>
2025-03-21 23:44:18 -0600

    make a commit of the jokes one part or two part jokes (WIP)

--- joke.js
+++ joke.js
@@ -3,3 +3,12 @@ function fetchJoke (catergary, title) {
 
 fetch(`https://v2.jokeapi.dev/joke/${category}?safe-mode`)
 }
+    .then(response => response.json())
+    .then(data => {
+        let jokeText =""; /* Variable to store the joke text*/
+    /* Check if it is a single part or two part joke*/
+        if (data.type === "single") {  
+            jokeText = data.joke;
+        } else {
+        }
+    })
