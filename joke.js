8b61afa8f4859fd9ede5e950d219819235dcce3c
Erintam23 <erin.tam@edu.sait.ca>
2025-03-24 13:58:00 -0600

    make a commit on fixing the error (WIP)

--- joke.js
+++ joke.js
@@ -1,16 +1,27 @@
-function fetchJoke (catergary, title) {
+function fetchJoke (cate, title) {
     document.getElementById("joke-title").innerText = title;
 
-fetch(`https://v2.jokeapi.dev/joke/${category}?safe-mode`)
-}
-     then(response => response.json())
+fetch(`https://v2.jokeapi.dev/joke/${cate}?safe-mode`)
+
+    .then(response => response.json())
     .then(data => {
-        let jokeText =""; /* Variable to store the joke text*/
+        let jokeText ="";
+ /* Variable to store the joke text*/
     /* Check if it is a single part or two part joke*/
         if (data.type === "single") {  
             jokeText = data.joke;
+            console.log (jokeText);
         } else {
             jokeText = `${data.setup} ... ${data.delivery}`;
+            console.log (jokeText);
         }
         document.getElementById("joke-text").innerText = jokeText;
     })
+    document.getElementById("joke-text").innerText = "Failed to load joke. Try again!";
+    console.error("Error fetching joke:", error);
+
+/* load a random joke when the page open */
+window.onload = function () {
+    fetchJoke ("Any, Joke Of The Day");
+
+}}
