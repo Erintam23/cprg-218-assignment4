7f7aa651320b53a717de71cc200d1dc76ba064d8
Erintam23 <erin.tam@edu.sait.ca>
2025-03-23 20:09:21 -0600

    make a commit of setting up the error of the page then generate joke error

--- joke.js
+++ joke.js
@@ -3,12 +3,14 @@ function fetchJoke (catergary, title) {
 
 fetch(`https://v2.jokeapi.dev/joke/${category}?safe-mode`)
 }
-    .then(response => response.json())
+     then(response => response.json())
     .then(data => {
         let jokeText =""; /* Variable to store the joke text*/
     /* Check if it is a single part or two part joke*/
         if (data.type === "single") {  
             jokeText = data.joke;
         } else {
+            jokeText = `${data.setup} ... ${data.delivery}`;
         }
+        document.getElementById("joke-text").innerText = jokeText;
     })
